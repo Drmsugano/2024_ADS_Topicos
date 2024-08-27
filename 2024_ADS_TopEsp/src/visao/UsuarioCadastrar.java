3dc49e59165db164b72ed0d9bac3c9a22ccd4383
Drmsugano <drmsugano3911@gmail.com>
2024-08-27 20:15:53 -0300

    Gerenciar Usuario e Principal

--- 2024_ADS_TopEsp/src/visao/UsuarioCadastrar.java
+++ 2024_ADS_TopEsp/src/visao/UsuarioCadastrar.java
@@ -128,6 +128,7 @@ public class UsuarioCadastrar extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCadastrarActionPerformed
